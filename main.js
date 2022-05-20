26c20e76131733c8917c70e008b50c18cbb45f2e
Arlene Santos <webdev.arlene@gmail.com>
2022-05-20 16:55:43 +0100

    v2 colors

--- main.js
+++ main.js
@@ -7,12 +7,12 @@ const showProductsGrid = (products) => {
         card.classList.add("col");
         card.innerHTML = `        
                         <div class="card h-100 shadow-sm">
-                            <div class="text-center p-3 ">
+                            <div class="text-center p-3">
                                 <img src="assets/images/${product.name}.png" class=" img-product" alt="imagem ilustrativa do produto: ${product.name}">
                             </div>
                             <div class="card-body ">
                                 <h1 class="h5 card-title">${product.name}</h1>
-                                <p class="card-text text-success fw-bold">R$ ${product.price}</p>
+                                <p class="card-text price">R$ ${product.price}</p>
                                 <div class="text-end">
                                     <a href="https://wa.me/5541988287796" class="btn btn-whatsapp rounded-whatsapp" target="_blank"><i class="bi bi-whatsapp" role="img" aria-label="Contato de Whatsapp"></i></a>
                                 </div>                                
@@ -75,7 +75,7 @@ const showCarousel = (products, cardsPerSlide) => {
                         </div>
                         <div class="card-body text-center">
                             <h1 class="h5 card-title">${products[j].name}</h1>
-                            <p class="card-text text-success fw-bold">R$ ${products[j].price}</p>                                                            
+                            <p class="card-text price">R$ ${products[j].price}</p>                                                            
                         </div>
                     </div>
                 </div>
